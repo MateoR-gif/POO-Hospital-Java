1da43c23f33624772c453b90a30323b21acf7b97
MateoR-gif <mateorendon008@gmail.com>
2023-04-18 19:57:14 -0500

    Inicio del Proyecto

---
+++ src/Doctor.java
@@ -0,0 +1,15 @@
+public class Doctor extends User{
+    private String speciality;
+
+    public Doctor(String name, String email){
+        super(name, email);
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
